28d8c871bb16577381848bf9aba0c62a9e970a20
Sabir5911 <115262495+Sabir5911@users.noreply.github.com>
2023-07-09 17:15:48 +0500

    j

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "./components/Hero";
-import Cards from "./components/Cards/Hero2";
+import Cards from "./components/Hero2";
 import Hero1 from "./components/Hero1";
-import Hero3 from "./components/Cards/Hero3";
+import Hero3 from "./components/Hero3";
 import Hero4 from "./components/Hero4";
 import Footer from "./components/Footer";
 export default async function Home() {
